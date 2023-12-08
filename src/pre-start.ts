ef998cc5c7b9ad485c1ebd8ee79dd5f86ed476a9
Adam Esbjörnsson <ades22@student.bth.se>
2023-12-08 21:08:41 +0100

    do not load .env file in simulation environment

--- src/pre-start.ts
+++ src/pre-start.ts
@@ -27,9 +27,11 @@ const args = parse<IArgs>({
 });
 
 // Set the env file
-const result2 = dotenv.config({
-    path: path.join(__dirname, `../env/${args.env}.env`),
-});
-if (result2.error) {
-    throw result2.error;
+if (process.env.NODE_ENV !== "simulation") {
+    const result2 = dotenv.config({
+        path: path.join(__dirname, `../env/${args.env}.env`),
+    });
+    if (result2.error) {
+        throw result2.error;
+    }
 }
